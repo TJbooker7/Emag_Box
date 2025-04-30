47fdf277bc50a5914529022a74fde511f0433f30
TJbooker7 <113914237+TJbooker7@users.noreply.github.com>
2025-04-30 14:01:30 -0500

    Update FunctionGen_WPT_NeoPixels.ino

--- FunctionGen_WPT_NeoPixels.ino
+++ FunctionGen_WPT_NeoPixels.ino
@@ -12,19 +12,16 @@
 #endif
 
 #define PIN 6
-
+int colmuns = 24;
 int c = 255; 
 
-
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(1,   1, 8, 24, PIN,
+//NeoMatrix number of pixels (8x24)
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(1,   1, 8, colmuns, PIN,
   NEO_TILE_TOP + NEO_TILE_LEFT + NEO_TILE_ROWS + NEO_TILE_PROGRESSIVE +
   NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
   NEO_GRB + NEO_KHZ800);
  // transfers a byte, a bit at a time, LSB first to the 9850 via serial DATA line
-// Adafruit_NeoMatrix matrix2 = Adafruit_NeoMatrix(1,   1, 8, 8, PIN,
-//   NEO_TILE_TOP + NEO_TILE_LEFT + NEO_TILE_ROWS + NEO_TILE_PROGRESSIVE +
-//   NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
-//   NEO_GRB + NEO_KHZ800);
+
 
  const uint16_t colors[] = {
   matrix.Color(255, 0, 0), // Red
@@ -81,9 +78,9 @@ pulseHigh(RESET);
 }
  
 void loop() {
-  sendFrequency(400000);  // freq
+  sendFrequency(400000);  // Important Line: sets frequency to 400,00Hz
 
- c=255;
+ c=255; //Important Line: sets in 16 bit (  link to colors:https://www.avrfreaks.net/s/topic/a5C3l000000UkUdEAK/t191803  )
 int adcValue=0;
   while(1){
   adcValue = analogRead(sensorPin);
@@ -103,10 +100,15 @@ delay(10);
   }
 
   void updateLEDs(float voltage, uint16_t c) {
-    int rowsToLight = (voltage-.37) / 0.19; // Determine how many rows should be lit
+    int rowsToLight = (voltage-.37) / (((4.63)/columns)-0.001); // Important line: voltage is maxed out at 5V by Arduino
+   // At the furthest range the arduino senses .37 volts taking this away from voltage will result in 0 rows of light.
+   //If we are at maximum we have (5-.37) / x
+   // which is 4.63 / x (we have 24 current columns)
+   // 4.63/24=0.19291666666666666666666666666667
+   
 
-    for (int y = 0; y < 24; y++) {
-        for (int x = 0; x < 8; x++) {
+    for (int y = 0; y < columns; y++) { //columns
+        for (int x = 0; x < 8; x++) { //rows
             if (y < rowsToLight) {
                 matrix.drawPixel(x, y, c);  // Turn on the pixel
             } else {
